a77fd5f5514025690d3f3e98bceb22c2fbf79e53
Nathan Howell <nhowell@alphaheavy.com>
2012-01-17 15:09:15 -0800

    Expose llvm::DisablePrettyStackTrace
    Conflicts:
    	base/LLVM/FFI/Support.hsc

--- base/cbits/support.cpp
+++ base/cbits/support.cpp
@@ -5,6 +5,7 @@
 
 #include "llvm-c/Core.h"
 #include "llvm/PassManager.h"
+#include "llvm/Support/PrettyStackTrace.h"
 #if HS_LLVM_VERSION >= 300
 # include "llvm/DefaultPasses.h"
 # include "llvm/Transforms/IPO/PassManagerBuilder.h"
@@ -88,3 +89,9 @@ void LLVMCreateStandardModulePasses(LLVMPassManagerRef PM,
                              HaveExceptions, InliningPass);
 #endif
 }
+
+void LLVMDisablePrettyStackTrace()
+{
+  llvm::DisablePrettyStackTrace = true;
+}
+
